aa9eb18bbdcf88e67fcaedf13a17cf45841bceb3
romanwdwd <romanwdwd@gmail.com>
2024-10-10 23:46:39 +0300

    save change

--- lab_4/api-gateway/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
+++ lab_4/api-gateway/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayServiceApplication {
+
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
